04d076a949b00c8a3d99a492d6921f5bf6d14ea9
TRL <Thomas@noxicsolutions.com>
2026-09-24 00:35:55 +0100

    fixed url handling for defs

--- src/open5e_test.py
+++ src/open5e_test.py
@@ -13,8 +13,8 @@ creatureFilter = {
 combinedFilter = {**creatureFilter, **versionFilter}
 
 #looking at the API, which is filtered with the creatureFilter, to discover the schema of the data within the API for creatures
-def creatureCheck(url, Filter):
-    response = requests.get(url,params=Filter)
+def creatureCheck(urlPlaceholder, Filter):
+    response = requests.get(urlPlaceholder,params=Filter)
     data = response.json()
     goblin = data["results"][0]
     print(data["count"])
