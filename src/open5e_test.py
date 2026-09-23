aca99e8d3db79b1a876b3c44f0873d6096e3d49e
TRL <Thomas@noxicsolutions.com>
2026-09-24 00:24:17 +0100

    add check for Open5e schema

--- src/open5e_test.py
+++ src/open5e_test.py
@@ -2,20 +2,32 @@ import requests
 
 url = "https://api.open5e.com/v2/creatures/"
 
-parameters = {
+versionFilter = {
     "document__key__in": "srd-2024"
 }
 
+creatureFilter = {
+    "name__iexact": "goblin"
+}
 
-
-next_url = url
-
-while next_url is not None:
-    response = requests.get(next_url, params=parameters)
+def creatureCheck(url, creatureFilter):
+    response = requests.get(url,params=creatureFilter)
     data = response.json()
+    goblin = data["results"][0]
+    print(data["count"])
+    for field, value in goblin.items():
+        print(field, ":", value)
 
-    for creature in data["results"]:
-        print(creature["Name"])
+def nameList(url,versionFilter):
+    next_url = url
 
-    next_url = data["next"]
-    
+    while next_url is not None:
+        response = requests.get(next_url, params=versionFilter)
+        data = response.json()
+
+        for creature in data["results"]:
+            print(creature["name"])
+
+        next_url = data["next"]
+
+creatureCheck(url, creatureFilter)
